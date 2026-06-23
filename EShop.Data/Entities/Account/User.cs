e9e6990f25d79c887d390e6b0a373620d1568f22
Narjes <Sunarjem@gmail.com>
2026-06-23 15:16:39 +0330

    feat: add domain entities (User and BaseEntity)

---
+++ EShop.Data/Entities/Account/User.cs
@@ -0,0 +1,16 @@
+﻿using EShop.Data.Entities.Common;
+
+namespace EShop.Data.Entities.Account
+{
+    public class User : BaseEntity
+    {
+        public string MobileNumber { get; set; }
+        public string MobileActivationNumber { get; set; }
+
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? PostCode { get; set; }
+
+    }
+}
---
+++ EShop.Data/Entities/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.Entities.Common
+{
+    public class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
