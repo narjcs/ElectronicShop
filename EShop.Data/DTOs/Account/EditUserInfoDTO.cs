5bc1b3421b93ced9c2447c1f8f160880b766e5aa
Narjes <Sunarjem@gmail.com>
2026-06-24 23:19:36 +0330

    feat(account): add EditUserInfoDTO for user profile management

---
+++ EShop.Data/DTOs/Account/EditUserInfoDTO.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.DTOs.Account
+{
+    public class EditUserInfoDTO
+    {
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string FullName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? Email { get; set; }
+
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(300, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Address { get; set; }
+
+        [Display(Name = "کد پستی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [MinLength(10, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+        public string PostCode { get; set; }
+    }
+}
