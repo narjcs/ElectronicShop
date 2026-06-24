79b4a1f1c597977a4926176d2c6598846b245e55
Narjes <Sunarjem@gmail.com>
2026-06-24 23:19:18 +0330

    feat(account): add MobileActivationDTO for phone verification

---
+++ EShop.Data/DTOs/Account/MobileActivationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.DTOs.Account
+{
+    public class MobileActivationDTO
+    {
+        [Display(Name = "کد فعالسازی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(5, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [MinLength(5, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+        public string ActivationCode { get; set; }
+    }
+}
