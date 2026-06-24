ecf4ffc0c9f38849199ff60c6368801db49d840f
Narjes <Sunarjem@gmail.com>
2026-06-24 23:14:18 +0330

    feat(account): add RegisterUserDTO for mobile-based signup

---
+++ EShop.Data/DTOs/Account/RegisterUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.DTOs.Account
+{
+    public class RegisterUserDTO
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(12, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [MinLength(12, ErrorMessage = "{0} نمی تواند کمتر از {1} کاراکتر باشد")]
+        public string MobileNumber { get; set; }
+    }
+}
