ebbb34f6a7f20b5e2d70d5b51f5843c4247e8ba9
Narjes <Sunarjem@gmail.com>
2026-06-25 13:12:12 +0330

    feat(account): add LoginDTO and UserDetailDTO

---
+++ EShop.Data/DTOs/Account/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Data.DTOs.Account
+{
+    public class LoginDTO
+    {
+        [Display(Name = "شماره موبایل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(11, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string MobileNumber { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
---
+++ EShop.Data/DTOs/Account/UserDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace EShop.Data.DTOs.Account
+{
+    public class UserDetailDTO
+    {
+        public long Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string MobileNumber { get; set; }
+        public string MobileActivationNumber { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? PostCode { get; set; }
+    }
+}
