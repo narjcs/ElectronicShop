cb6c5968d08f5338e6669fe4cfc9fbbb934c57bb
Narjes <Sunarjem@gmail.com>
2026-06-28 21:48:57 +0330

    feat(repository): add generic repository

---
+++ EShop.Data/Repository/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿
+
+using EShop.Data.Entities.Common;
+
+namespace EShop.Data.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        public Task AddEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeEntities(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRengeEntities(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePermanent(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRangeEntities(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRangeEtities(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetEntityById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EShop.Data/Repository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Data.Entities.Common;
+
+namespace EShop.Data.Repository
+{
+    public interface IGenericRepository<TEntity>: IAsyncDisposable where TEntity : BaseEntity 
+    {
+        IQueryable<TEntity> GetQuery();
+        Task<TEntity> GetEntityById(long id);
+        Task AddEntity (TEntity entity);
+        Task AddRangeEntities (List<TEntity> entities);
+        void EditEntity (TEntity entity);
+        void DeleteEntity (TEntity entity);     //Soft Delete, IsDelete = true
+        void DeleteRangeEntities (List<TEntity> entities);
+        Task DeletePermanent (TEntity entity);  //Hard Delete
+        Task SaveAsync();
+
+    }
+}
