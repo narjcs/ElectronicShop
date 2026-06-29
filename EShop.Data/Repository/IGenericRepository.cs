1d82ea42edc738ac03a2a8e3f3b96b9c7e6f5d5c
Narjes <Sunarjem@gmail.com>
2026-06-30 00:31:17 +0330

    refactor(repository): update IGenericRepository interface

--- EShop.Data/Repository/IGenericRepository.cs
+++ EShop.Data/Repository/IGenericRepository.cs
@@ -5,13 +5,13 @@ namespace EShop.Data.Repository
     public interface IGenericRepository<TEntity>: IAsyncDisposable where TEntity : BaseEntity 
     {
         IQueryable<TEntity> GetQuery();
-        Task<TEntity> GetEntityById(long id);
+        Task<TEntity?> GetEntityById(long id);
         Task AddEntity (TEntity entity);
         Task AddRangeEntities (List<TEntity> entities);
         void EditEntity (TEntity entity);
         void DeleteEntity (TEntity entity);     //Soft Delete, IsDelete = true
         void DeleteRangeEntities (List<TEntity> entities);
-        Task DeletePermanent (TEntity entity);  //Hard Delete
+        void DeletePermanent (TEntity entity);  //Hard Delete
         Task SaveAsync();
 
     }
